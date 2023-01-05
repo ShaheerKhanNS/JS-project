cdc4950346ca971a7ffe9ee300bd04dda282728f
Shaheer Khan NS <khanshaheer43@gmail.com>
2023-01-05 13:05:03 +0530

    Second commit

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const addProduct = async function () {
     const parentNode = document.getElementById("table");
     const data = await axios({
       method: "POST",
-      url: "https://crudcrud.com/api/c22b774f46094a8bb7c9247c96fb8686/product",
+      url: "https://crudcrud.com/api/2e0ca4e1d2d649fba87d3dedbf4e25fc/product",
       data: {
         productName,
         productPrice,
@@ -19,17 +19,20 @@ const addProduct = async function () {
       },
     });
     const template = `
-      <tr>
-      <td>${productName}</td>
-      <td>${productPrice}</td>
-      <td>${productCategory}</td>
+      <tr id='row_${data.data._id}'>
+      <td id='name_${data.data._id}'>${productName}</td>
+      <td id='price_${data.data._id}'>${productPrice}</td>
+      <td id='category_${data.data._id}'>${productCategory}</td>
       <td>
       <button type="button" data-set ='${data.data._id}' id="button--dlt" onclick="deleteRow(this)">Delete</button>
+      <button type="button" data-set ='${data.data._id}' id="button--edt" onclick="edit(this)">Edit</button>
       </td>
       </tr>`;
     parentNode.innerHTML = parentNode.innerHTML + template;
     clearField();
-  } catch (err) {}
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
 document.getElementById("button").addEventListener("click", addProduct);
@@ -40,27 +43,50 @@ const deleteRow = async (el) => {
   const tr = el.parentNode.parentNode;
   await axios({
     method: "DELETE",
-    url: `https://crudcrud.com/api/c22b774f46094a8bb7c9247c96fb8686/product/${id}`,
+    url: `https://crudcrud.com/api/2e0ca4e1d2d649fba87d3dedbf4e25fc/product/${id}`,
   });
   td.deleteRow(tr);
+};
+
+const edit = async function (el) {
+  try {
+    console.log(`I have edited this😎`);
+    const id = el.dataset.set;
+    const name = document.getElementById(`name_${id}`).textContent;
+    const price = document.getElementById(`price_${id}`).textContent;
+    const category = document.getElementById(`category_${id}`).textContent;
+    const row = document.getElementById(`row_${id}`);
+    row.remove();
+
+    document.getElementById("product").value = name;
+    document.getElementById("price").value = price;
+    document.getElementById("category").value = category;
+    await axios({
+      method: "DELETE",
+      url: `https://crudcrud.com/api/2e0ca4e1d2d649fba87d3dedbf4e25fc/product/${id}`,
+    });
+  } catch (err) {
+    err.message;
+  }
 };
 
 const retrieveData = async () => {
   const parentNode = document.getElementById("table");
   const data = await axios({
     method: "GET",
-    url: "https://crudcrud.com/api/c22b774f46094a8bb7c9247c96fb8686/product",
+    url: "https://crudcrud.com/api/2e0ca4e1d2d649fba87d3dedbf4e25fc/product",
   });
 
   data.data.forEach((el) => {
     const { productName, productPrice, productCategory, _id } = el;
     const template = `
-      <tr>
-      <td>${productName}</td>
-      <td>${productPrice}</td>
-      <td>${productCategory}</td>
+      <tr id='row_${_id}'>
+      <td id='name_${_id}'>${productName}</td>
+      <td id='price_${_id}'>${productPrice}</td>
+      <td id='category_${_id}'>${productCategory}</td>
       <td>
       <button type="button" data-set ='${_id}' id="button--dlt" onclick="deleteRow(this)">Delete</button>
+      <button type="button" data-set ='${_id}' id="button--edt" onclick="edit(this)">Edit</button>
       </td>
       </tr>`;
     parentNode.innerHTML = parentNode.innerHTML + template;
